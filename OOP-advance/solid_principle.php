5a3d874953bf013504ac04293d95f974a7e07942
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-03-28 14:52:32 +0600

    rename some file

---
+++ OOP-advance/solid_principle.php
@@ -0,0 +1,15 @@
+<?php
+
+// SOLID is an acronym that represents five principles of object-oriented design that aim to make software systems more understandable, flexible, and maintainable.
+
+// Here's a brief explanation of each of the SOLID principles:
+
+// 1. Single Responsibility Principle (SRP): This principle states that a class should have only one reason to change. In other words, a class should have only one responsibility, so that if there is a change in the requirements, it should only affect that single responsibility and not the entire class.
+
+// 2. Open/Closed Principle (OCP): This principle states that a class should be open for extension but closed for modification. This means that we should be able to add new functionality to a class without changing its existing code.
+
+// 3. Liskov Substitution Principle (LSP): This principle states that a subclass should be able to be substituted for its superclass without causing any problems or unexpected behavior in the system.
+
+// 4. Interface Segregation Principle (ISP): This principle states that clients should not be forced to depend on interfaces they do not use. In other words, we should create specific interfaces that are tailored to the needs of individual clients rather than having a single monolithic interface that is used by all clients.
+
+// 5. Dependency Inversion Principle (DIP): This principle states that high-level modules should not depend on low-level modules, but both should depend on abstractions. This means that we should define abstractions that separate the higher-level modules from the lower-level modules, and the dependencies should be on these abstractions rather than on the specific implementations.
